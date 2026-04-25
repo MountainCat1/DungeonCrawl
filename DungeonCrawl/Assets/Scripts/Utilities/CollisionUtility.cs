3fae956dc5e3dfa70368f0404c6e8a2d43005663
Tomasz Banaś <tomasz.banas123@gmail.com>
2026-04-26 00:21:45 +0200

    add ui extensions and utilities

---
+++ DungeonCrawl/Assets/Scripts/Utilities/CollisionUtility.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class CollisionUtility
+    {
+        public static bool IsObstacle(GameObject go)
+        {
+            return go.layer == LayerMask.NameToLayer("Obstacles");
+        }
+        
+        public static bool IsWall(GameObject go)
+        {
+            return go.layer == LayerMask.NameToLayer("Walls");
+        }
+
+        public static int BlockingVisionLayerMask => LayerMask.GetMask("Obstacles", "Walls");
+        public static int UnwalkableLayerMask => LayerMask.GetMask("Obstacles", "Walls");
+        
+        public static List<T> GetObjectsInRadius<T>(Vector2 position, float radius, T[] ignore = null)
+        {
+            List<T> hits = new();
+            HashSet<T> ignoreSet = ignore != null ? new(ignore) : new();
+
+            Collider2D[] results = new Collider2D[50];
+            int size = Physics2D.OverlapCircleNonAlloc(position, radius, results);
+
+            for (int i = 0; i < size; i++)
+            {
+                var hitCollider = results[i];
+                var hit = hitCollider.GetComponent<T>();
+
+                
+                if (hit != null && !ignoreSet.Contains(hit))
+                {
+                    hits.Add(hit);
+                }
+            }
+
+            return hits;
+        }
+    }
+}
---
+++ DungeonCrawl/Assets/Scripts/Utilities/Localization/LocalizationStringExtensions.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public static class LocalizationStringExtensions
+    {
+        public static string Localize(this string fullKey)
+        {
+            if (string.IsNullOrWhiteSpace(fullKey))
+            {
+                return fullKey;
+            }
+
+            return LocalizationHelper.L(fullKey);
+        }
+
+        public static string Localize(this string fullKey, params object[] args)
+        {
+            if (string.IsNullOrWhiteSpace(fullKey))
+            {
+                Debug.LogWarning("Localization key is null or empty.");
+                return fullKey;
+            }
+
+            return LocalizationHelper.L(fullKey, args);
+        }
+    }
+}
